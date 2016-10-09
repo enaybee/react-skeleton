8134db6b441047806a916e41186eab3d89bb4697
enaybee <enaybee@mail.com>
2016-10-09 00:02:38 -0400

    react-skeleton

---
+++ src/components/Listitem.jsx
@@ -0,0 +1,11 @@
+var react = require('react');
+var ListItem = React.createClass({
+    render: function(){
+        return(
+            <li>
+                <h4>{this.props.ingredient}</h4>
+            </li>
+        );
+    }
+});
+module.exports = ListItem;
